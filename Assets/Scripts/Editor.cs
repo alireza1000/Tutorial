bbc61be9901de64814c874c4cd33b218363f483f
alireza1000 <alireza_ghalyanforoushan@yahoo.com>
2021-09-20 12:13:11 +0430

    Text

--- Assets/Scripts/Editor.cs
+++ Assets/Scripts/Editor.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 
@@ -8,22 +9,20 @@ using UnityEngine;
     public class Editor : MonoBehaviour
     {
         [SerializeField] [Range(1f, 50f)] private float grid = 10f;
+
+        private TextMesh _textMesh;
+
         void Update()
         {
             Vector3 editPos;
-
             editPos.x = Mathf.RoundToInt(transform.position.x / grid) * grid;
-
             editPos.z = Mathf.RoundToInt(transform.position.z / grid) * grid;
-            
 
             transform.position = new Vector3(editPos.x, 0f, editPos.z);
 
-            float k = Mathf.RoundToInt(transform.position.x / grid) * grid;
-        
+            _textMesh = GetComponentInChildren<TextMesh>();
 
-    }
-
-       
+            _textMesh.text = editPos.x + "," + editPos.z;
+        }
     }
 
